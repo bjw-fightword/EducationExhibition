f9123e7610330b52f99981f5ab9988dcd589859c
bjw-fightword <641605665@qq.com>
2016-05-05 17:09:39 +0800

    移动

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+	/*报名参会弹出框*/
+	$('.sign-up').click(function(){
+		if($('.fix-content').css('display')=='none'){
+			$('.fix-content').slideDown();
+		}else{
+			$('.fix-content').slideUp();
+		}
+	})
+	$(".contact").click(function(event){
+		event.stopPropagation();
+      	$("#mymodal").slideDown();
+    });
+    $(".btn").click(function(event){
+    	event.stopPropagation();
+      	$("#mymodal1").modal("toggle");
+      	$("#mymodal").hide();
+    });
+   $(".bt-fix-left").click(function(event){
+   		event.stopPropagation();
+   		$("#mymodal2").modal("toggle");
+   })
+})
